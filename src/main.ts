a682672b451ad244c6961bbecdc4830c746f62ac
davidetoscano129 <toscano.davidew@gmail.com>
2025-05-14 17:02:52 +0200

    Milestone 2 completed

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,23 @@ type Person = {
   biography: string;
   image: string;
 }
+
+type ActressNationality = 
+|"American"
+|"British"
+|"Australian"
+|"Israeli-American"
+|"South African"
+|"French"
+|"Indian"
+|"Israeli"
+|"Spanish"
+|"South Korean"
+|"Chinese"
+
+
+type Actress = Person & {
+  most_famous_movies: [string, string, string];
+  awards: string;
+  nationality: ActressNationality
+}
